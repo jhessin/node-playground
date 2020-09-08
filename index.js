23ee3e13799060e88a1c2f22c648a5bff1942956
Jim Hessin <jhessin@gmail.com>
2020-09-08 10:25:57 -0600

    reading files

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // index.js
 
-var lodash = require('lodash');
-var capitalString =
+let lodash = require('lodash');
+let capitalString =
   lodash.capitalize('hello world!');
-console.log(capitalString); // Hello world!
+// console.log(capitalString); // Hello world!
+
+const fs = require('fs');
+
+fs.writeFile('hello.txt', 'Hey there world', err => {
+  if (err) throw err;
+  console.log('successfully saved');
+});
